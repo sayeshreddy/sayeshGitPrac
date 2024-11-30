834af21f738a8a9a52a139267fef92eb8bd45d28
sayeshreddy <pallapolu.sayeshreddy@gmail.com>
2024-11-30 14:04:33 +0530

    new line

--- src/test/java/gitsayesh/AppTest.java
+++ src/test/java/gitsayesh/AppTest.java
@@ -4,5 +4,6 @@ public class AppTest
 	public static void main(String[] args) {
 		System.out.println("first push to git");
 		System.out.println("second push by reddy merge conflict");
+		System.out.println("get conflict");
 	}    
 }
